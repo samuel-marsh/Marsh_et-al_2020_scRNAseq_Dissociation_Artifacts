55346bd9a189ac16accbbce71819129b56928f39
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2020-05-28 21:12:01 -0400

    Update prep project file micro analysis

--- 02_Microglia-Myeloid Protocol Comparison (Mouse)/01_Prep Project.R
+++ 02_Microglia-Myeloid Protocol Comparison (Mouse)/01_Prep Project.R
@@ -14,10 +14,8 @@ install.dates("tidyverse", "2019-02-28")
 
 install.dates("BiocManager", "2019-02-28")
 
-install.dates(c("lars", "fpc", "dtw", "Hmisc", "doSNOW", "foreach", "hdf5r"), "2019-02-28")
+install.dates(c("cowplot", "ROCR", "mixtools", "lars", "ica", "tsne", "Rtsne", "fpc", "ape", "pbapply", "igraph", "RANN", "irlba", "gplots", "dtw", "SDMTools", "plotly", "Hmisc", "ggridges", "metap", "lmtest", "fitdistrplus", "png", "doSNOW", "reticulate", "foreach", "hdf5r", "RcppEigen", "RcppProgress"), "2019-02-28")
 
 # Install Seurat from CRAN archive
-    #wget https://cran.r-project.org/src/contrib/Archive/Seurat/Seurat_2.3.4.tar.gz
+#wget https://cran.r-project.org/src/contrib/Archive/Seurat/Seurat_2.3.4.tar.gz
 install.packages("Seurat_2.3.4.tar.gz", repos = NULL, type = "source")
-
-
