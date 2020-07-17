ebf7f2f6888161f418434865521a29886fe9fe3b
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2020-07-17 07:02:42 -0400

    Add liger plotting

--- 08_Misc/Custom Functions.R
+++ 08_Misc/Custom Functions.R
@@ -20,3 +20,42 @@ clusterLouvainJaccard = function(object, resolution = 0.1, k.param=30, n.iter = 
   object@clusters = temp.seurat@ident
   return(object)
 }
+
+
+
+# Custom Plotting by Sample LIGER
+DimPlot_All_Samples_Liger <- function(liger_object, cell_data_column = "dataset", color = "black", num_col = NULL, label_size = 14, ...){
+  # Pull dataset info
+  column_list <- as.character(unique(liger_object@cell.data[, "dataset"]))
+  # Pull reduc coordinates
+  reduc_coordinates <- data.frame(liger_object@tsne.coords)
+  colnames(reduc_coordinates) <- c("tsne1", "tsne2")
+  x_axis <- c(min(reduc_coordinates[, 1]),
+              max(reduc_coordinates[, 1]))
+  y_axis <- c(min(reduc_coordinates[, 2]),
+              max(reduc_coordinates[, 2]))
+  # Plotting Function
+  Dim_by_Sample_Liger <- function(liger_object, column_list){
+    # Pull Cell Barcodes
+    cell_barcodes <- row.names(liger_object@cell.data)[which(liger_object@cell.data[, cell_data_column] == column_list)]
+    
+    # Subset liger
+    temp_liger <- suppressWarnings(subsetLiger(object = liger_object, cells.use = cell_barcodes, remove.missing = FALSE))
+    
+    # Plot
+    plot <- plotByDatasetAndCluster(temp_liger, return.plots = TRUE, do.legend = TRUE, text.size = 6)
+    plot <- plot[[1]] +
+      scale_color_manual(values = "black") +
+      ggtitle(paste(column_list)) +
+      theme(plot.title = element_text(hjust = 0.5, size = label_size),
+            legend.position = "none") +
+      xlim(x_axis) +
+      ylim(y_axis)
+  }
+  # Iterate and create plot list
+  all_plots <- map(column_list, ~Dim_by_Sample_Liger(liger_object, .x))
+  # Wrap Plots into single output
+  wrap_plots(all_plots, ncol = num_col)
+}
+
+
