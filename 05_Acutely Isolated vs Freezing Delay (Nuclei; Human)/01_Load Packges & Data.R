0f9bccea6475ab075c3161cbcd098e2d8543341a
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2020-09-25 07:38:38 -0400

    create project folder acute human

---
+++ 05_Acutely Isolated vs Freezing Delay (Nuclei; Human)/01_Load Packges & Data.R
@@ -0,0 +1,13 @@
+# Load Packages -----------------------------------------------------------
+library(Seurat)
+library(liger)
+library(NNLM)
+library(stringr)
+
+install.packages("~/Desktop/Bioinformatics Tools/R Source Packages/Seurat_3.1.5.tar.gz", repos = NULL, type = "source")
+install.packages("~/Desktop/Bioinformatics Tools/R Source Packages/Seurat_2.3.4.tar.gz", repos = NULL, type = "source")
+
+
+# Load Data ---------------------------------------------------------------
+
+
